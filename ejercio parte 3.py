a7b7230c6c7f956781869b687bb9cc07f08c6e76
Yaninna Alvarez <134446812+Yaninna137@users.noreply.github.com>
2023-09-04 19:12:47 -0300

    avance en lenguaje python

--- ejercio parte 3.py
+++ ejercio parte 3.py
@@ -1,11 +1,13 @@
 import time
+import keyboard
 import random as rd
 class Playlist:
     def __init__(self,nombre,arreglo):
         self.nombre = nombre                                 # Debe poseer un atributo "nombre". Este será la referencia al nombre de la lista de reproducción
         self.listas_contenedor_de_song = arreglo              # Un atributo que contendrá las canciones; se espera que sea un arreglo de strings
-        self.Estado = ['Reproduciendo','Pausa','Detenido']   # Un atributo que representará el estado. Los estados pueden ser: reproduciendo, pausa y detenido
-        self.indice_cancion = None
+        self.Estado = 'Detenido'   # Un atributo que representará el estado. Los estados pueden ser: reproduciendo, pausa y detenido
+        self.indice_cancion = 0           # se inicializa en 0 por defaout , ya que al iniciar este tendra 0 , para iniciar la play list                           # Un atributo que indica el índice de la canción que se está reproduciendo
+
 
     def Añadir_cancion(self,cancion):                       # Un método que permitirá añadir una canción.
         self.listas_contenedor_de_song.append(cancion)      ## usando una funcion append se agregaran las nuevas caniones.
@@ -17,57 +19,104 @@ class Playlist:
         for x in range(len(self.listas_contenedor_de_song)):
             print(x,'¬. ',self.listas_contenedor_de_song[x])
 
-    def Comienza_a_reproducir_cancion(self,arreglo):
-        for x in range(len(arreglo)):                       # reproducira la cancion
-            time.sleep(10)
-    def Selccionar_una_cancion_a_reproducir(self,indice):
-        return self.listas_contenedor_de_song[indice]             # con el indice selecciona la posiscio ah reproducir
-
-    def Pausar_cancion(self,indice):
-        return 
-    def Detener_la_reproduccion():
-        pass
-    def Pasar_sig_cancion():
-        pass
-    def Retroceder_cancion_anterior(self,cancion):
-        index = self.listas_contenedor_de_song.index(cancion)
-        return index - 1                                          # retornar el indice de la anterior cancion
-    def Reproducir_una_cancion_aleatoria(self):
-        # ''' (si ya está reproduciendo una canción, 
-        #     al seleccionar una canción aleatoria no puede 
-        #     reproducir la que ya se estaba reproduciendo).'''
-        len_lista = len(self.listas_contenedor_de_song)
-        indice_de_reproducion = [i for i in range(len_lista)]
-        # desordenar lista  // arrglar con un algoritmo , eficas 
-        lista = []
-        i = 0
-        while i <= len(lista):
-            numero_aleatorio = rd.randrange(len_lista)
-            if numero_aleatorio not in lista:
-                lista.append(numero_aleatorio)
-                i += 1
-        return lista                    # retornar una lista con los indices de cada cancion pero desordenada
+    def Comienza_Reproducir_cancion(self):                          # comienza a reproducir una canción
+        self.Estado = 'Reproduciendo'
+        return self.listas_contenedor_de_song[self.indice_cancion]
     
-    def Ver_estado_Playlist(self,num_estado_pregunta):
-        if num_estado_pregunta == 0:
-            return 'Esta playlist esta en {}'.format(self.Estado[0])
-        if num_estado_pregunta == 1:
-            return 'Esta playlist esta en {}'.format(self.Estado[1])
-        if num_estado_pregunta == 2:
-            return 'Esta playlist esta en {}'.format(self.Estado[2])      
-          
+    def Selccionar_una_cancion_a_reproducir(self,indice):
+        self.indice_cancion = indice                           # Cambiar el valor de la actual reproducion y colocar ah reproducir el nuevo
+
+    def Pausar_cancion(self):
+        self.Estado = 'Pausado'
+        print('La cancion se encuentra pausada')
+        m = input('Enter para dejar la pausa')
+        self.Estado = 'Reproduccion'
+    def Detener_la_reproduccion(self):
+        self.Estado = 'Detenido'
+
+    def Pasar_sig_cancion(self):
+        self.indice_cancion += 1
+        print('Siguiente...')
+    def Retroceder_cancion_anterior(self):
+        self.indice_cancion -= 1                                # retornar el indice de la anterior cancion
+        print('Retrocediendo...')
+    def Reproducir_una_cancion_aleatoria(self):
+        lista = self.listas_contenedor_de_song
+        len_lista = len(self.listas_contenedor_de_song)
+        i = 0
+        while len(lista) > i:
+            numero_aleatorio = rd.randrange(len_lista)
+            aux = lista[i]
+            lista[i] = lista[numero_aleatorio]
+            lista[numero_aleatorio] = aux
+            i += 1          
+    def Ver_estado_Playlist(self):
+        return 'Esta playlist esta en {}'.format(self.Estado)      
     def Ver_cancion_reproduciendo(self,indice_reproducion):
-        return f'La cancion que se esta REPRODUCIENDO es: {self.listas_contenedor_de_song[indice_reproducion]}'
+        return f'La cancion que se esta REPRODUCIENDO es: {self.listas_contenedor_de_song[self.indice_cancion]}'
+    #METODO PARA ELIMINAR OBJETO
+    def __del__(self):
+        print('objeto destruido')
 
-canciones = ['musica pop','musica clasica','musica rok','musica electrica','musica jazz']
+# canciones = ['musica pop','musica clasica','musica rok','musica electrica','musica jazz']
+# My_musica = Playlist('XXLL',canciones)
+canciones = ['musica pop','musica clasica','musica jazz']
 My_musica = Playlist('XXLL',canciones)
-My_musica.Comienza_a_reproducir_cancion()
+def menu():
+    print('Bienvenido. Para manejar tu Playlist tienes los suiguintes opciones')
+    print('A.Mostrar todas las canciones.')
+    print('B.Eliminar una canción.')
+    print('C.Añadir una canción')
 
-def comando_de_playlist(objeto):
-    input('REproducir(0) , Pausar una cancion(1), Detener(2), Sig.Cancion(3), Retroceder(4)','R.ALEATORIA' )
+    print('1.Reproducir una canción(0)')
+    print('2.Reproducir una canción aleatoria ')
+    print('3.Seleccionar una canción a reproducion(1)')
+    print('4.Pausar una canción')
+    print('5.Detener la reproducción')
+    print('6.Siguiente canción')
+    print('7retroceder a la canción anterior')
+    print('8.estado de la playlist')
+    print('9.Ver la canción que se está reproduciendo')
+def menum():
+    print("Bienvenido <(II)>")
+    print("1.reproducir")
+    print(" 'p' para pausar play list")
+def comando_r(objeto):
+    objeto.Estado = 'Reproduccion'
+    tiempo_total_c = len(canciones) * 10
+    i = j = 0
+    TiempoC = 10
+    while i < TiempoC and j <tiempo_total_c:
 
+        if keyboard.is_pressed('p'):                         # Al precionar la tecla p se pausara la playlist
+            objeto.Pausar_cancion()
+        if objeto.Estado == 'Reproduccion':
+            objeto.listas_contenedor_de_song[objeto.indice_cancion]
+            time.sleep(1)
+            print(i)
+            i += 1
+            j += 1
+        if keyboard.is_pressed('p'):
+            objeto.Pausar_cancion()
+        if i == 10:
+            i = 0
+            print('musica anterior: {}'.format(objeto.listas_contenedor_de_song[objeto.indice_cancion]))
+            objeto.indice_cancion += 1
+            if j == tiempo_total_c:
+                objeto.indice_cancion = 0
 
-# start = False
-# while not start:
-#     print(f'Bienvenido a tu {My_musica.nombre}')
-#     input('')
+end = False
+while not end:
+    menum()
+    r = input('Seeccion:')
+    if r == '1':
+        while True:
+            comando_r(My_musica)
+            print('Musica reproducida')
+            r = input('Volver a R:(si)')
+            if r == 'no':
+                break
+            
+                
+    if r == '0':
+        break
